5c42a79ebaa89226ff96d9f5b532e736307daaaa
Manoel Vilela <manoel_vilela@engineer.com>
2017-12-12 16:37:08 -0300

    Cria classe Main wrapper em rentalcar.gui

--- rentalcar/gui.py
+++ rentalcar/gui.py
@@ -3,12 +3,16 @@ from rentalcar import forms
 import sys
 from PyQt5 import QtWidgets
 
+class Main(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        ui = forms.Ui_MainWindow()
+        ui.setupUi(self)
+
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-    ui = forms.Ui_MainWindow()
-    ui.setupUi(window)
-
+    window = Main()
 
     window.show()
     sys.exit(app.exec_())
