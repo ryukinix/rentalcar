b26adeda46943dccb9df80b6a2cec3701b1c84f1
Manoel Vilela <manoel_vilela@engineer.com>
2017-12-12 20:16:44 -0300

    Adiciona FreeWidget no __init__ de forms

--- rentalcar/forms/__init__.py
+++ rentalcar/forms/__init__.py
@@ -13,3 +13,4 @@ from .AddWidget import Ui_AddWidget
 from .FetchWidget import Ui_FetchWidget
 from .RentWidget import Ui_RentWidget
 from .DeleteWidget import Ui_DeleteWidget
+from .FreeWidget import Ui_FreeWidget
