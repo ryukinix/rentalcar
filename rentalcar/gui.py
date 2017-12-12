1bc196355b3b4622cf703b291757c99431194dc9
Manoel Vilela <manoel_vilela@engineer.com>
2017-12-12 20:01:32 -0300

    Adiciona algumas docstrings em rentalcar.gui

--- rentalcar/gui.py
+++ rentalcar/gui.py
@@ -1,18 +1,28 @@
+# coding: utf-8
+
 from rentalcar import forms
 
 import sys
 from PyQt5 import QtWidgets
 
 class Main(QtWidgets.QMainWindow):
+    "Janela principal contendo todos os menus e informações"
     def __init__(self):
         super().__init__()
         ui = forms.Ui_MainWindow()
         ui.setupUi(self)
 
-
-def main():
+def create():
+    "Cria a aplicação e a janela"
     app = QtWidgets.QApplication(sys.argv)
     window = Main()
+
+    return app, window
+
+
+def main():
+    "Realiza todos os procedimentos para abrir a aplicação"
+    app, window = create()
 
     window.show()
     sys.exit(app.exec_())
